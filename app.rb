9fc39d0277838e67d11336a908d68f4fe0a6b0af
Shohei Yamasaki <shoyamas@yahoo-corp.jp>
2018-11-16 16:22:57 +0900

    メモを保存できるようにした

--- app.rb
+++ app.rb
@@ -1,16 +1,35 @@
 require './app/memo'
+require 'pstore'
 
 class App < Sinatra::Application
   configure do
     set :root, Dir.pwd
+    set :storage, Dir.pwd + '/storage'
   end
 
-  get '/' do
+  get '/memo/new' do
     erb :index
   end
 
+  get '/' do
+    store = PStore.new(settings.storage)
+
+    @memo = store.transaction do
+      store['memo']
+    end
+
+    erb :completed
+  end
+
+
   post '/' do
     @memo = Memo.create(params[:title], params[:body])
+
+    store = PStore.new(settings.storage)
+    store.transaction do
+      store['memo'] = @memo
+    end
+
     erb :completed
   end
 end
